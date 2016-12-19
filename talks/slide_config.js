2d3dacf77dabb399bc498453007245840e64f1e6
Wen-Chun Lin <lovecankill@gmail.com>
2016-12-19 12:51:00 +0800

    Add intro clojure.spec talk

---
+++ talks/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: '當我們討論 spec 我們討論的是？', 
+     subtitle: '談談 clojure.spec', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/clojure-logo.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'lovecankill', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: 'http://github.com/cataska', 
+   }]
+};
