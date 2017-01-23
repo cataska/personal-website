f13c82d4d4ab92791f8c157373ab5fae35d3ffb3
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-23 22:45:45 +0800

    Add advanced clojure.spec slide

---
+++ talks/advanced-spec/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: '關於 spec，我說的其實是...', 
+     subtitle: '進階 clojure.spec', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/clojure-logo.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'lovecankill', 
+     company: '', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: 'http://github.com/cataska', 
+   }]
+};
